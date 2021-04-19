96ca1db766fc5421374e177f8b31382ed3ab6f0b
pedroobando <pedroobando@hotmail.com>
2021-04-19 07:52:00 -0400

    authenticacion -

---
+++ src/app/common/Errorcomponent.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Button, Header, Segment } from 'semantic-ui-react';
+
+const ErrorComponent = () => {
+  const { error } = useSelector((state) => state.async);
+
+  return (
+    <Segment placeholder>
+      <Header textAlign="center" content={error?.message || 'Oops - we have an error'} />
+      <Button
+        as={Link}
+        to="/events"
+        primary
+        style={{ marginTop: '20px' }}
+        content="Return to events page"
+      />
+    </Segment>
+  );
+};
+
+export default ErrorComponent;
