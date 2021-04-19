f48f5cdd6f5a9cbc052f8657ed10f1d182fc096c
pedroobando <pedroobando@hotmail.com>
2021-04-19 07:29:10 -0400

    StartPage - Ok

--- src/features/start/StartPage.jsx
+++ src/features/start/StartPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Button, Container, Header, Icon, Image, Segment } from 'semantic-ui-react';
 
-const StartPage = () => {
-  return <div>Hola Tetas Lovers</div>;
+const StartPage = ({ history }) => {
+  return (
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            style={{ marginBotton: 12, width: 200, marginRight: 50 }}
+          />
+          Teta Lovers
+        </Header>
+        <Button size="huge" inverted onClick={() => history.push('/events')}>
+          Vamos por una <Icon name="right arrow" inverted />
+        </Button>
+      </Container>
+    </Segment>
+  );
 };
 
 export default StartPage;
