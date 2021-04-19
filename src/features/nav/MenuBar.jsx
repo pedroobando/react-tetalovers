08ee4d4db05f4f7dcd8a3fbb5cbf4185212b473d
pedroobando <pedroobando@hotmail.com>
2021-04-19 08:16:46 -0400

    MenuBar -

--- src/features/nav/MenuBar.jsx
+++ src/features/nav/MenuBar.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button, Container, Menu } from 'semantic-ui-react';
+import { useSelector } from 'react-redux';
+import SignedOutMenu from './SignedOutMenu';
+import SignedInMenu from './SignedInMenu';
 
 const MenuBar = () => {
-  return <div></div>;
+  const { authenticated } = useSelector((state) => state.auth);
+
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item exact as={NavLink} to="/" header>
+          <img src="/assets/logo.png" alt="logo" style={{ marginRight: '15px' }} />
+          Re-Vents
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/events" name="Events" />
+        <Menu.Item as={NavLink} to="/sandbox" name="Sandbox" />
+        {authenticated && (
+          <Menu.Item as={NavLink} to="/createEvent">
+            <Button positive inverted content="Create Event" />
+          </Menu.Item>
+        )}
+        {authenticated ? <SignedInMenu /> : <SignedOutMenu />}
+      </Container>
+    </Menu>
+  );
 };
 
 export default MenuBar;
