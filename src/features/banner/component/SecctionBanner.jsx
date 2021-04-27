1b4c5e394ca28966c63a358aba46dcc32c567939
Pedro Obando <pedroobando@hotmail.com>
2021-04-27 15:09:11 -0400

    LoadingComponent en HomePage

--- src/features/banner/component/SecctionBanner.jsx
+++ src/features/banner/component/SecctionBanner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Button, Icon, Image } from 'semantic-ui-react';
+// import { Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 
@@ -13,8 +13,6 @@ const SecctionBanner = () => {
   const dispatch = useDispatch();
   const { banners } = useSelector((state) => state.banner);
   const { loading } = useSelector((state) => state.async);
-
-  const { authenticated } = useSelector((state) => state.auth);
 
   useFirestoreCollection({
     query: () => listenToBannersFromFirestore(),
@@ -39,16 +37,10 @@ const SecctionBanner = () => {
       <Slider {...settings}>
         {banners.map((promo, idx) => (
           <Link key={idx} to={promo.linkURL.toString().length > 1 ? promo.linkURL : '/home'}>
-            <Image src={promo.imagenURL} alt={promo.name} />
+            <img src={promo.imagenURL} alt={promo.name} />
           </Link>
         ))}
       </Slider>
-      {authenticated && (
-        <Button basic icon labelPosition="right" id="buttonchange">
-          Change
-          <Icon name="edit outline" />
-        </Button>
-      )}
     </section>
   );
 };
--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Container } from 'semantic-ui-react';
+import LoadingComponent from '../../app/layout/LoadingComponent';
+
 import SecctionCategory from './SecctionCategory';
 import SecctionBanner from '../banner/component/SecctionBanner';
 import SecctionBrand from './SecctionBrand';
@@ -7,8 +10,10 @@ import SecctionProduct from './SecctionProduct';
 import SecctionFooter from './SecctionFooter';
 
 const HomePage = () => {
+  const { loading } = useSelector((state) => state.async);
   return (
     <>
+      {loading && <LoadingComponent />}
       <SecctionBanner />
       <Container>
         <SecctionCategory />
