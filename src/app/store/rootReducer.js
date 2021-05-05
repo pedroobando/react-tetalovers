31e8b08c312fb97ee26a39df2712b60d724e6095
pedroobando <pedroobando@hotmail.com>
2021-05-05 18:28:38 -0400

    rootReducer - remove bannerReducer

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import testReducer from '../../features/sandox/testRedux';
 import asyncReducer from '../async/asyncReducer';
 import authReducer from '../../features/auth/authReducer';
-import bannerReducer from '../../features/banner/bannerReducer';
+// import bannerReducer from '../../features/banner/bannerReducer';
 import categoryReducer from '../../features/category/categoryReducer';
 import modalReducer from '../common/modals/modalReducer';
 import productReducer from '../../features/product/productReducer';
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
   modals: modalReducer,
   auth: authReducer,
   async: asyncReducer,
-  banner: bannerReducer,
+  // banner: bannerReducer,
   category: categoryReducer,
   product: productReducer,
 });
